33396e83ecec215ddc25a1adb1735be18f0f785d
Dmitrenko <Dias64@mail.ru>
2019-03-06 17:01:50 +0300

    ДЗ по уроку 7 - "Крестики-нолики" в ООП

---
+++ src/main/java/TickTackToe_OOP/Game/Game.java
@@ -0,0 +1,45 @@
+package main.java.TickTackToe_OOP.Game;
+
+import javax.swing.*;
+
+public class Game {
+    private GameBoard board;         // ссылка на игровое поле
+    private GamePlayer[] gamePlayers = new GamePlayer[2];  // массив игроков
+    private int playersTurn = 0;     // индекс текущего игрока
+
+    public Game() {
+        this.board = new GameBoard(this);  // передача текущего экземляра игры
+    }
+
+    public void initGame() {
+        gamePlayers[0] = new GamePlayer(true, 'X');  // реальный игрок
+        gamePlayers[1] = new GamePlayer(false, '0');
+    }
+
+    /**
+     * Метод передачи хода
+     */
+    void passTurn() {
+        if (playersTurn == 0)
+            playersTurn = 1;
+        else
+            playersTurn = 0;
+    }
+
+    /**
+     * Получение объекта текущего игрока
+     * @return GamePlayer объект игрока
+     */
+    GamePlayer getCurrentPlayer() {
+        return gamePlayers[playersTurn];
+    }
+
+    /**
+     * Метод показа popup-a для пользователя
+     * @param messageText - текст сообщения
+     */
+    void showMessage(String messageText) {
+        JOptionPane.showMessageDialog(board, messageText);
+    }
+
+}
---
+++ src/main/java/TickTackToe_OOP/MainClass.java
@@ -0,0 +1,11 @@
+package main.java.TickTackToe_OOP;
+
+import main.java.TickTackToe_OOP.Game.Game;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Game gameInstance = new Game();
+        gameInstance.initGame();
+
+    }
+}
